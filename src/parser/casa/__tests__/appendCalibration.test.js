277dd3d7818844f3ed6b6a7767d3bff43b2495bc
Luc Patiny <luc@patiny.com>
2024-02-27 16:48:41 +0100

    feat: parse calibrations

---
+++ src/parser/casa/__tests__/appendCalibration.test.js
@@ -0,0 +1,40 @@
+import { appendCalibration } from '../appendCalibration';
+
+test('appendCalibration', () => {
+  const data = `Calib M = 10.1 A = 20.1 BE ADD
+Calib M = 20.2 A = 40.2 BE ADD
+Calib M = 1.1 A = 5.1 ADD`
+
+  const calibrations = [];
+  const lines = data.split(/\r?\n/);
+  for (const line of lines) {
+    if (line.startsWith('Calib')) {
+      appendCalibration(calibrations, line);
+    }
+  }
+
+  expect(calibrations).toHaveLength(3);
+  expect(calibrations).toStrictEqual([
+    {
+      "after": 20.1,
+      "before": 10.1,
+      "bindingEnergyShift": 10.000000000000002,
+      "kind": "bindingEnergy",
+      "kineticEnergyShift": -10.000000000000002,
+    },
+    {
+      "after": 40.2,
+      "before": 20.2,
+      "bindingEnergyShift": 20.000000000000004,
+      "kind": "bindingEnergy",
+      "kineticEnergyShift": -20.000000000000004,
+    },
+    {
+      "after": 5.1,
+      "before": 1.1,
+      "bindingEnergyShift": -3.9999999999999996,
+      "kind": "kineticEnergy",
+      "kineticEnergyShift": 3.9999999999999996,
+    },
+  ])
+});
--- src/parser/casa/appendCalibration.js
+++ src/parser/casa/appendCalibration.js
@@ -3,9 +3,22 @@ export function appendCalibration(calibrations, line) {
   // Calib M = 281.1700 A = 284.8 BE ADD
   // calibration may be present on many lines
 
-  let fields = line.match(/Calib (M = .*) (A = [^ ]*) (.*)/);
+  let fields = line.match(/Calib M = (?<before>.*) A = (?<after>[^ ]*) (?<rest>.*)/);
   if (!fields) {
     throw new Error(`appendCalibration fails on: ${line}`);
   }
+  if (!fields.groups.rest.includes('ADD')) {
+    throw new Error(`appendCalibration fails because line does not contain ADD: ${line}`);
+  }
+  calibration.before = Number(fields.groups.before)
+  calibration.after = Number(fields.groups.after)
+  if (fields.groups.rest.includes('BE')) {
+    calibration.bindingEnergyShift = calibration.after - calibration.before;
+    calibration.kind = 'bindingEnergy';
+  } else {
+    calibration.bindingEnergyShift = calibration.before - calibration.after;
+    calibration.kind = 'kineticEnergy';
+  }
+  calibration.kineticEnergyShift = -calibration.bindingEnergyShift
   calibrations.push(calibration);
 }
