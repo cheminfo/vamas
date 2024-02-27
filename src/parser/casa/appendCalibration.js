f5286e16bd47a44972596aca3496a586a711ffde
Luc Patiny <luc@patiny.com>
2024-02-27 16:51:54 +0100

    chore: fix prettier

--- src/parser/casa/appendCalibration.js
+++ src/parser/casa/appendCalibration.js
@@ -3,15 +3,19 @@ export function appendCalibration(calibrations, line) {
   // Calib M = 281.1700 A = 284.8 BE ADD
   // calibration may be present on many lines
 
-  let fields = line.match(/Calib M = (?<before>.*) A = (?<after>[^ ]*) (?<rest>.*)/);
+  let fields = line.match(
+    /Calib M = (?<before>.*) A = (?<after>[^ ]*) (?<rest>.*)/,
+  );
   if (!fields) {
     throw new Error(`appendCalibration fails on: ${line}`);
   }
   if (!fields.groups.rest.includes('ADD')) {
-    throw new Error(`appendCalibration fails because line does not contain ADD: ${line}`);
+    throw new Error(
+      `appendCalibration fails because line does not contain ADD: ${line}`,
+    );
   }
-  calibration.before = Number(fields.groups.before)
-  calibration.after = Number(fields.groups.after)
+  calibration.before = Number(fields.groups.before);
+  calibration.after = Number(fields.groups.after);
   if (fields.groups.rest.includes('BE')) {
     calibration.bindingEnergyShift = calibration.after - calibration.before;
     calibration.kind = 'bindingEnergy';
@@ -19,6 +23,6 @@ export function appendCalibration(calibrations, line) {
     calibration.bindingEnergyShift = calibration.before - calibration.after;
     calibration.kind = 'kineticEnergy';
   }
-  calibration.kineticEnergyShift = -calibration.bindingEnergyShift
+  calibration.kineticEnergyShift = -calibration.bindingEnergyShift;
   calibrations.push(calibration);
 }
