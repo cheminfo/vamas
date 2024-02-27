0d4f0d66ae18cf2af19decaaf9516776993d1fc3
Luc Patiny <luc@patiny.com>
2024-02-27 14:45:43 +0100

    wip: deal with calibration

---
+++ src/__tests__/appendCalibration.test.js
@@ -0,0 +1,8 @@
+test('appendCalibration', () => {
+  const data = `Calib M = 10.1 A = 20.1 BE ADD
+Calib M = 20.2 A = 40.2 BE ADD
+Calib M = 1.1 A = 5.1 ADD`;
+
+  const bindingEnergyShift = 26;
+  const kineticEnergyShift = -26;
+});
--- src/parser/casa/appendCalibration.js
+++ src/parser/casa/appendCalibration.js
@@ -1,6 +1,8 @@
 export function appendCalibration(calibrations, line) {
   let calibration = {};
-  // Calib M = 281.1700 A = 284.8 BE AD
+  // Calib M = 281.1700 A = 284.8 BE ADD
+  // calibration may be present on many lines
+
   let fields = line.match(/Calib (M = .*) (A = [^ ]*) (.*)/);
   if (!fields) {
     throw new Error(`appendCalibration fails on: ${line}`);
