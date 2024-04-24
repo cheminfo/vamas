0e816ce31b082d6f87fd268398db24a50fbcf2bf
Luc Patiny <luc@patiny.com>
2024-04-24 16:35:21 +0200

    wip: improve parseCASE

---
+++ src/parser/casa/__tests__/appendComponent.test.js
@@ -0,0 +1,149 @@
+import { appendComponent } from '../appendComponent';
+
+test('appendComponent', () => {
+  const data = `
+CASA comp (*C 1s CO*) (*LA(1.4,1.4,100)*) Area 12064.956 1e-020 433826 -1 1 MFWHM 2.1060023 0.3960396 9.9009901 -1 1 Position 1205.191 1198.8295 1210.6853 -1 1 RSF 0.278 MASS 12.011 INDEX -1 (*C 1s*) CONST (**) UNCORRECTEDRSF 0.278
+CASA comp (*C 1s*) (*LA(1,643)*) Area 1572.7938 1e-020 433826 -1 1 MFWHM 2.6387221 0.52774442 13.19361 -1 1 Position 1205.69 1198.8295 1210.6853 -1 1 RSF 0.278 MASS 12.011 INDEX -1 (*C 1s*) CONST (**) UNCORRECTEDRSF 0.278
+CASA comp (*C 1s CC*) (*GL(30)*) Area 35666.616 1e-020 623362.6 -1 1 MFWHM 1.1910554 0.3960396 9.9009901 -1 1 Position 1205.9046 1198.8295 1210.6853 -1 1 RSF 0.278 MASS 12.011 INDEX -1 (*C 1s*) CONST (**) UNCORRECTEDRSF 0.278
+CASA comp (*C 1s C=O*) (*SGL(10)*) Area 3971.5812 1e-020 433826 -1 1 MFWHM 1.9803865 0.3960396 9.9009901 -1 1 Position 1202.355 1198.8295 1210.6853 -1 1 RSF 0.278 MASS 12.011 INDEX -1 (*C 1s*) CONST (**) UNCORRECTEDRSF 0.278
+CASA comp (*O 1s metal oxides*) (*GL(30)*) Area 42915.464 1e-020 1583614.4 -1 1 MFWHM 1.0947654 0.68 17 -1 1 Position 961.1203 954.38267 963.88622 -1 1 RSF 0.78 MASS 15.9994 INDEX -1 (*O 1s*) CONST (**) UNCORRECTEDRSF 0.78
+CASA comp (*O 1s orgs*) (*GL(30)*) Area 21457.732 1e-020 1583614.4 0 0.5 MFWHM 2.3895444 0.68 17 4 2.1827 Position 959.3078 0 0 0 -1.8125 RSF 0.78 MASS 15.9994 INDEX -1 (*O 1s*) CONST (**) UNCORRECTEDRSF 0.78
+ `;
+
+  const components = [];
+  const lines = data.split(/\r?\n/);
+  for (const line of lines) {
+    if (line.startsWith('CASA comp')) {
+      appendComponent(components, line);
+    }
+  }
+
+  const componentIDs = components.map((c) => c.componentID);
+  expect(componentIDs).toEqual([
+    'C 1s CO',
+    'C 1s',
+    'C 1s CC',
+    'C 1s C=O',
+    'O 1s metal oxides',
+    'O 1s orgs'
+  ]);
+
+  const shapes = components.map((c) => c.shape);
+  expect(shapes).toStrictEqual([
+    {
+      kind: 'lorentzianAsymmetric',
+      options: { alpha: 1.4, beta: 1.4, m: 100 }
+    },
+    {
+      kind: 'lorentzianAsymmetric',
+      options: { alpha: 1, beta: 643 }
+    },
+    { kind: 'gaussianLorentzianProduct', options: { mixingRatio: 30 } },
+    { kind: 'pseudoVoigt', options: { mu: 10 } },
+    { kind: 'gaussianLorentzianProduct', options: { mixingRatio: 30 } },
+    { kind: 'gaussianLorentzianProduct', options: { mixingRatio: 30 } },
+  ])
+
+  const areas = components.map((c) => c.area);
+  expect(areas).toStrictEqual([
+    {
+      value: 12064.956,
+      lowerBound: 1e-20,
+      upperBound: 433826,
+    },
+    {
+      value: 1572.7938,
+      lowerBound: 1e-20,
+      upperBound: 433826,
+    },
+    {
+      value: 35666.616,
+      lowerBound: 1e-20,
+      upperBound: 623362.6,
+    },
+    {
+      value: 3971.5812,
+      lowerBound: 1e-20,
+      upperBound: 433826,
+    },
+    {
+      value: 42915.464,
+      lowerBound: 1e-20,
+      upperBound: 1583614.4,
+    },
+    {
+      value: 21457.732,
+      lowerBound: 1e-20,
+      upperBound: 1583614.4,
+      constrain: { linkedComponent: 0, factor: 0.5 }
+    }
+  ])
+
+  const fwhms = components.map((c) => c.fwhm);
+  expect(fwhms).toStrictEqual([
+    {
+      value: 2.1060023,
+      lowerBound: 0.3960396,
+      upperBound: 9.9009901,
+    },
+    {
+      value: 2.6387221,
+      lowerBound: 0.52774442,
+      upperBound: 13.19361,
+    },
+    {
+      value: 1.1910554,
+      lowerBound: 0.3960396,
+      upperBound: 9.9009901,
+    },
+    {
+      value: 1.9803865,
+      lowerBound: 0.3960396,
+      upperBound: 9.9009901,
+    },
+    {
+      value: 1.0947654,
+      lowerBound: 0.68,
+      upperBound: 17,
+    },
+    {
+      value: 2.3895444,
+      lowerBound: 0.68,
+      upperBound: 17,
+      "constrain": {
+        "factor": 2.1827,
+        "linkedComponent": 4,
+      },
+    }]
+  )
+
+  const positions = components.map((c) => c.position);
+  expect(positions).toStrictEqual([
+    { value: 1205.191, lowerBound: 1198.8295, upperBound: 1210.6853 },
+    { value: 1205.69, lowerBound: 1198.8295, upperBound: 1210.6853 },
+    { value: 1205.9046, lowerBound: 1198.8295, upperBound: 1210.6853 },
+    { value: 1202.355, lowerBound: 1198.8295, upperBound: 1210.6853 },
+    { value: 961.1203, lowerBound: 954.38267, upperBound: 963.88622 },
+    {
+      value: 959.3078,
+      lowerBound: 0,
+      upperBound: 0,
+      constrain: { linkedComponent: 0, shift: -1.8125 }
+    }
+  ])
+
+  expect(components[0]).toStrictEqual({
+    componentID: 'C 1s CO',
+    shape: {
+      kind: 'lorentzianAsymmetric',
+      options: { alpha: 1.4, beta: 1.4, m: 100 }
+    },
+    area: { value: 12064.956, lowerBound: 1e-20, upperBound: 433826 },
+    fwhm: { value: 2.1060023, lowerBound: 0.3960396, upperBound: 9.9009901 },
+    position: { value: 1205.191, lowerBound: 1198.8295, upperBound: 1210.6853 },
+    relativeSensitivityFactor: 0.278,
+    atomicMass: 12.011,
+    groupIndex: -1,
+    uncorrectedRSF: 0.278
+  })
+});
--- src/parser/casa/appendComponent.js
+++ src/parser/casa/appendComponent.js
@@ -1,10 +1,42 @@
+
+/**
+Fitting / each 'component' is a fitted lineshape, and can be from a different element while on a given orbital, for instance Ruthenium has peaks in the C1s region. The component will often have overlapping lineshapes
+
+"CASA comp (*C 1s compA*) (*GL(30)*) Area 2447.211 1e-020 223027.03 -1 1 MFWHM 0.72109706 0.21 5.25 -1 1 Position 1203.2974 1198.6232 1207.3108 -1 1 RSF 0.278 MASS 12.011 INDEX -1 (*C 1s*) CONST (**) UNCORRECTEDRSF 0.278"
+This relates to the fittings (comments on the constraints later on):
+
+componentID: C 1s compA
+lineshape: GL(30)
+area: 2447.211
+areaConstraint lb: 1e-020
+areaConstraint ub: 223027.03
+fwhm: 0.72109706
+fwhmConstraint lb: 0.21
+fwhmConstraint ub: 5.25
+position: 1203.2974 // in kinetic energy eV
+positionConstraint lb: 1198.6232 // in kinetic energy eV
+positionConstraint ub: 1207.3108 // in kinetic energy eV
+relativeSensitivityFactor: 0.278
+mass: 12.011
+index: -1	// index allows to group different components to plot their manifold
+uncorrectedRSF: 0.278
+
+Constraints: the constraints can either be absolute, in which case the value to constraint is directly followed by the constaints, e.g.:
+Position 1203.4193 1202.686 1207.3108
+
+or relative to another component, with this weird notation:
+"Position 1202.261 0 0 2 -1.05017"
+here the position is separated from the constaints by "0 0", then "2" indicates the component to which is is constrained, here the 2nd component (third line of the comp, i.e, starting from 0). And the last number "-1.05017" is the shift (in the kinetic energy scale)
+*/
+
+
 export function appendComponent(components, line) {
   // CASA comp (*Mo 3d MoS2 2H*) (*LA(1.53,243)*) Area 230.36971 1e-020 2327991 -1 1 MFWHM 0.88528218 0.2 2 -1 1 Position 1257.22 1257.02 1257.22 -1 1 RSF 10.804667 MASS 95.9219 INDEX -1 (*Mo 3d*) CONST (**) UNCORRECTEDRSF 9.5
   let component = {};
   const componentRegex = new RegExp(
     [
       /CASA comp /,
-      /\((?<name>.*)\) /,
+      /\(\*(?<componentID>.*)\*\) /,
       /\((?<shape>[^ ]*)\) /,
       /(?<area>Area .*)/,
       /(?<fwhm>MFWHM .*)/,
@@ -24,15 +56,14 @@ export function appendComponent(components, line) {
     throw new Error(`appendComponent fails on: ${line}`);
   }
   component = {
-    name: fields.groups.name,
+    componentID: fields.groups.componentID,
     shape: parseShape(fields.groups.shape),
-    area: parseArea(fields.groups.area),
-    fwhm: parseFWHM(fields.groups.fwhm),
-    position: parsePosition(fields.groups.position),
-    rsf: parseRSF(fields.groups.rsf),
-    mass: parseMass(fields.groups.mass),
-    index: parseIndex(fields.groups.index),
-    const: parseConst(fields.groups.const),
+    area: parseMultiplicationConstrain(fields.groups.area),
+    fwhm: parseMultiplicationConstrain(fields.groups.fwhm),
+    position: parseKEShiftConstrain(fields.groups.position),
+    relativeSensitivityFactor: parseRSF(fields.groups.rsf),
+    atomicMass: parseMass(fields.groups.mass),
+    groupIndex: parseIndex(fields.groups.index),
     uncorrectedRSF: parseUncorrectedRSF(fields.groups.uncorrectedRSF),
   };
   components.push(component);
@@ -46,14 +77,27 @@ function parseShape(value) {
   let options = {};
   let kind;
   switch (parts[0]) {
-    case 'LA':
-      kind = `${parts[0]}`;
-      options.asymmetry = Number(parts[1]);
-      options.extension = Number(parts[2]);
+    case 'LA': // http://www.casaxps.com/help_manual/manual_updates/LA_Lineshape.pdf
+      kind = 'lorentzianAsymmetric';
+      options.alpha = Number(parts[1]);
+      options.beta = Number(parts[2]);
+      if (parts[3] !== undefined) {
+        options.m = Number(parts[3]);
+      }
       break;
-    case 'GL':
-      kind = `${parts[0]}`;
-      options.unknown = Number(parts[1]);
+    case 'GL': // http://www.casaxps.com/help_manual/line_shapes.htm
+      kind = 'gaussianLorentzianProduct';
+      options.mixingRatio = Number(parts[1]);
+      break;
+    case 'SGL': // https://docs.mantidproject.org/nightly/fitting/fitfunctions/PseudoVoigt.html
+      kind = 'pseudoVoigt';
+      options.mu = Number(parts[1]);
+      break;
+    case 'LF':
+      kind = 'gaussianLorentzianSum';
+      break;
+    case 'LS':
+      kind = 'gaussianLorentzianSum';
       break;
     default:
       throw Error(`appendComponent: unknown shape: ${parts[0]}`);
@@ -63,32 +107,46 @@ function parseShape(value) {
     options,
   };
 }
-function parseArea(value) {
-  let parts = value.split(' ');
-  return {
+function parseMultiplicationConstrain(value) {
+  const parts = value.split(' ');
+  const result = {
     value: Number(parts[1]),
-    from: Number(parts[2]),
-    to: Number(parts[3]),
-    unknown1: Number(parts[4]),
-    unknown2: Number(parts[5]),
+    lowerBound: Number(parts[2]),
+    upperBound: Number(parts[3]),
   };
+  const constrain = {
+    linkedComponent: Number(parts[4]), // line number, no constrain if -1
+    factor: Number(parts[5]), // multiplication factor of the value in the linked line
+  }
+  if (constrain.linkedComponent >= 0) {
+    result.constrain = constrain;
+  }
+  return result
 }
-function parseFWHM(value) {
-  let parts = value.split(' ');
-  return {
+
+function parseKEShiftConstrain(value) {
+  const parts = value.split(' ');
+  const result = {
     value: Number(parts[1]),
-    from: Number(parts[2]),
-    to: Number(parts[3]),
-    unknown1: Number(parts[4]),
-    unknown2: Number(parts[5]),
+    lowerBound: Number(parts[2]),
+    upperBound: Number(parts[3]),
   };
+  const constrain = {
+    linkedComponent: Number(parts[4]), // line number, no constrain if -1
+    shift: Number(parts[5]), // shift of the value in the linked line
+  }
+  if (constrain.linkedComponent >= 0) {
+    result.constrain = constrain;
+  }
+  return result
 }
+
 function parsePosition(value) {
   let parts = value.split(' ');
   return {
     value: Number(parts[1]),
-    from: Number(parts[2]),
-    to: Number(parts[3]),
+    lowerBound: Number(parts[2]),
+    upperBound: Number(parts[3]),
     unknown1: Number(parts[4]),
     unknown2: Number(parts[5]),
   };
@@ -105,9 +163,6 @@ function parseIndex(value) {
   let parts = value.split(' ');
   return Number(parts[1]);
 }
-function parseConst(value) {
-  return value;
-} // We do not really know what this value means and hence just parse the string
 function parseUncorrectedRSF(value) {
   let parts = value.split(' ');
   return Number(parts[1]);
